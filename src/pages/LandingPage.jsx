a3cc3cdd427e414bb9ed3e67c9d7e91bade74a78
Malin Henningsson <malinhenningsson@live.se>
2022-09-25 20:18:50 +0200

    epns

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 import * as S from "../components/styles"
 import ShareCampaignPage from "./ShareCampaignPage"
-// import * as EpnsAPI from "@epnsproject/sdk-restapi"
-// import * as ethers from "ethers"
+import * as EpnsAPI from "@epnsproject/sdk-restapi"
+import * as ethers from "ethers"
 
 const Container = styled.main`
   display: flex;
@@ -34,37 +34,37 @@ const ButtonContainer = styled.div`
 `
 
 const LandingPage = ({ setPage }) => {
-  // const PK = "972faff600e7883dba2ce52a67c4fc34c417a72147d69fb9fc5d00ed30758ccd"
-  // const Pkey = `0x${PK}`
-  // const signer = new ethers.Wallet(Pkey)
+  const PK = "972faff600e7883dba2ce52a67c4fc34c417a72147d69fb9fc5d00ed30758ccd"
+  const Pkey = `0x${PK}`
+  const signer = new ethers.Wallet(Pkey)
 
-  // const sendNotification = async () => {
-  //   try {
-  //     const apiResponse = await EpnsAPI.payloads.sendNotification({
-  //       signer,
-  //       type: 3, // target
-  //       identityType: 2, // direct payload
-  //       notification: {
-  //         title: `titlez`,
-  //         body: `bodyz`,
-  //       },
-  //       payload: {
-  //         title: `payload title`,
-  //         body: `sample msg body`,
-  //         cta: "www.paypal.com",
-  //         img: "",
-  //       },
-  //       recipients: "eip155:42:0xb1d97f8DBe9B4C971622df75F0D02Bb50955b526", // recipient address
-  //       channel: "eip155:42:0xb1d97f8DBe9B4C971622df75F0D02Bb50955b526", // your channel address
-  //       env: "prod",
-  //     })
+  const sendNotification = async () => {
+    try {
+      const apiResponse = await EpnsAPI.payloads.sendNotification({
+        signer,
+        type: 3, // target
+        identityType: 2, // direct payload
+        notification: {
+          title: `titlez`,
+          body: `bodyz`,
+        },
+        payload: {
+          title: `payload title`,
+          body: `sample msg body`,
+          cta: "www.paypal.com",
+          img: "",
+        },
+        recipients: "eip155:42:0xb1d97f8DBe9B4C971622df75F0D02Bb50955b526", // recipient address
+        channel: "eip155:42:0xb1d97f8DBe9B4C971622df75F0D02Bb50955b526", // your channel address
+        env: "prod",
+      })
 
-  //     // apiResponse?.status === 204, if sent successfully!
-  //     console.log("API repsonse: ", apiResponse)
-  //   } catch (err) {
-  //     console.error("Error: ", err)
-  //   }
-  // }
+      // apiResponse?.status === 204, if sent successfully!
+      console.log("API repsonse: ", apiResponse)
+    } catch (err) {
+      console.error("Error: ", err)
+    }
+  }
 
   return (
     <>
